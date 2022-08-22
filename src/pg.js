8b7696b53416c0ba8a1489aff29c07e60c7d06a5
Muhammad Bin Aamir <95607161+MBA-CB@users.noreply.github.com>
2022-08-22 12:38:48 +0500

    refactor: get db details from .env

--- src/pg.js
+++ src/pg.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 // Database Connection
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "root",
-  host: "192.168.100.30",
-  database: "ndau",
-  password: "12345",
-  port: "5432",
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database:process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   // ssl: true
 });
 pool.connect(async (err) => {
